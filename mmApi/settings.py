2860569bce40f7d8a79767f835f1c949a802d126
Roman Sokolenko <sokolero@yandex.ru>
2021-02-19 21:52:48 +0300

    settings.py исправлено Debug=True

--- mmApi/settings.py
+++ mmApi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e#aq-r7@)erxvt%&c6l5c7pbv_fot7gajrjlos%)7h)jzokd2#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
